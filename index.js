cc27ae8e413b9e5bb3d0837631189302796f7bc3
IvoSa92 <ivo_sauter@web.de>
2023-12-06 21:54:55 +0100

    created function for creating new books with title,author,pages and read status

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = function () {
+  this.read = read;
+  this.info = function () {
     return this.readStatus === "yes"
       ? "I have already read this book!"
       : "I´havent read this book yet!";
@@ -12,3 +13,20 @@ function Book(title, author, pages, read) {
 }
 
 let book1 = new Book("Ivo", "sauter", 220, "yes");
+let book2 = new Book("Miriam", "Rossig", 110, "yes");
+let book3 = new Book("Nadia", "Sauter", 300, "no");
+
+console.log(book1, book2, book3);
+
+/*
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      const readStatus =
+        this.read === "yes" ? "I read that book" : "I dont read that book";
+      return `${title} by ${author}, ${pages} pages, ${readStatus}`;
+    };
+  }*/
