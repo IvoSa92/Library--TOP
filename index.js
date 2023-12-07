61a0a2e00880651eb1899973c4b867f58bef17a6
IvoSa92 <ivo_sauter@web.de>
2023-12-07 11:29:17 +0100

    created the function that the form appears after pressing the new book button, created function to add the inputs to a new book and the new book to the library

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ let myLibrary = [
 
 const libraryTable = document.querySelector(".library-table");
 const newBookBtn = document.querySelector(".new-book-button");
+const bookInputForm = document.querySelector(".book-input-form");
+const titleInput = document.querySelector("#book-title");
+const authorInput = document.querySelector("#author");
+const pagesInput = document.querySelector("#pages");
+const submitBtn = document.querySelector("#submit");
+const readStatusCheckbox = document.querySelector("#read-status");
 
 // Functions:
 
@@ -27,7 +33,7 @@ function Book(title, author, pages, status) {
   this.pages = pages;
   this.status = status;
   this.info = function () {
-    return this.readStatus === "yes"
+    return this.status === "yes"
       ? "I have already read this book!"
       : "I´haven´t read this book yet!";
   };
@@ -38,31 +44,19 @@ function addBookToLibrary(title, author, pages, status) {
   myLibrary.push(book);
 }
 
-newBookBtn.addEventListener("click");
+newBookBtn.addEventListener("click", () => {
+  bookInputForm.style.display = "flex";
+});
 
-/*
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function () {
-      const readStatus =
-        this.read === "yes" ? "I read that book" : "I dont read that book";
-      return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-    };
-  }
-  
-  addBookToLibrary("aa","aa",22,"no")
-  
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
+  const pages = pagesInput.value;
+  const status = readStatusCheckbox.checked ? "yes" : "no";
 
-  function showLibrary() {
-  const test = document.createElement("div");
-  const title = document.createElement("h1");
-  title.textContent = book1.title;
-  title.classList.add("title");
-  test.appendChild(title);
-  test.classList.add("newBookEntry");
-  libraryTable.appendChild(test);
-}
-  */
+  addBookToLibrary(title, author, pages, status);
+  bookInputForm.reset();
+  bookInputForm.style.display = "none";
+  console.log(myLibrary);
+});
